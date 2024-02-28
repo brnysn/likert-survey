e6f267ad6781c250ef1312d24cd50470057ca018
Onur Bilen Laçın <73442869+onurbilenlacin@users.noreply.github.com>
2024-02-28 11:39:16 +0300

    revision for bugs

--- app.js
+++ app.js
@@ -11,39 +11,39 @@ const getData = async () => {
 const displayForm = async () => {
     let data = await getData();
 
+    let groupedData = [];
     function combineQuestionsByCategory(data) {
-        return data.reduce((combinedArray, currentItem) => {
-            const foundCategoryIndex = combinedArray.findIndex(
-                (item) =>
-                    item.category === currentItem.category &&
-                    "en" === currentItem.language
-            );
-            if (foundCategoryIndex !== -1) {
-                combinedArray[foundCategoryIndex].questions.push({
-                    language: currentItem.language,
-                    factor: currentItem.factor,
-                    question: currentItem.question,
-                    unit_name: currentItem.unit_name,
-                    unit_link: currentItem.unit_link
-                });
-            } else {
-                combinedArray.push({
-                    category: currentItem.category,
-                    questions: [
-                        {
-                            language: currentItem.language,
-                            factor: currentItem.factor,
-                            question: currentItem.question,
-                            unit_name: currentItem.unit_name,
-                            unit_link: currentItem.unit_link
-                        }
-                    ]
-                });
+        return data.forEach(function (item) {
+            var foundLanguage = groupedData.find(function (group) {
+                return group.language === item.language;
+            });
+
+            if (!foundLanguage) {
+                foundLanguage = {
+                    language: item.language,
+                    categories: []
+                };
+                groupedData.push(foundLanguage);
             }
-            return combinedArray;
-        }, []);
+
+            var foundCategory = foundLanguage.categories.find(function (
+                category
+            ) {
+                return category.category === item.category;
+            });
+
+            if (!foundCategory) {
+                foundCategory = {
+                    category: item.category,
+                    items: []
+                };
+                foundLanguage.categories.push(foundCategory);
+            }
+            foundCategory.items.push(item);
+        });
     }
     data = combineQuestionsByCategory(data);
+    data = groupedData.find((item) => item.language === "en").categories;
 
     const formContainer = document.querySelector(".likert-survey");
     let currentPage = 0;
@@ -57,7 +57,7 @@ const displayForm = async () => {
 
         const category = data[pageIndex];
 
-        category.questions.forEach((question, key) => {
+        category.items.forEach((question, key) => {
             const questionDiv = document.createElement("div");
             questionDiv.classList.add("statement-custom-radios");
 
@@ -67,35 +67,35 @@ const displayForm = async () => {
                     <div>
                         <input type="radio" id=${pageIndex}-${key}-1 class="color-1" name=mark-${pageIndex}-${key} value="5" />
                         <label for=${pageIndex}-${key}-1 >
-                            <span><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg" alt="Checked Icon" /></span>
+                            <span><img src="check.svg" alt="Checked Icon" /></span>
                             <p>Very Satisfied</p>
                         </label>
                     </div>
                     <div>
                         <input type="radio" id=${pageIndex}-${key}-2  class="color-2" name=mark-${pageIndex}-${key} value="4" />
                         <label for=${pageIndex}-${key}-2 >
-                            <span><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg" alt="Checked Icon" /></span>
+                               <span><img src="check.svg" alt="Checked Icon" /></span>
                             <p>Satisfied</p>
                         </label>
                     </div>
                     <div>
                         <input type="radio" id=${pageIndex}-${key}-3  class="color-3" name=mark-${pageIndex}-${key} value="3" />
                         <label for=${pageIndex}-${key}-3 >
-                            <span><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg" alt="Checked Icon" /></span>
+                               <span><img src="check.svg" alt="Checked Icon" /></span>
                             <p>Neutral</p>
                         </label>
                     </div>
                     <div>
                         <input type="radio" id=${pageIndex}-${key}-4 class="color-4" name=mark-${pageIndex}-${key} value="2" />
                         <label for=${pageIndex}-${key}-4>
-                            <span><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg" alt="Checked Icon" /></span>
+                               <span><img src="check.svg" alt="Checked Icon" /></span>
                             <p>Unsatisfied</p>
                         </label>
                     </div>
                     <div>
                         <input type="radio" id=${pageIndex}-${key}-5 class="color-5" name=mark-${pageIndex}-${key} value="1" />
                         <label for=${pageIndex}-${key}-5>
-                            <span><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg" alt="Checked Icon" /></span>
+                               <span><img src="check.svg" alt="Checked Icon" /></span>
                             <p>Very Unsatisfied</p>
                         </label>
                     </div>
@@ -123,7 +123,6 @@ const displayForm = async () => {
 
         // Check if all questions are answered before enabling the nextButton
         function checkAllQuestionsAnswered() {
-            console.log("Checking if all questions are answered");
             const allQuestionsAnswered = inputReferences.every((inputArray) => {
                 return Array.from(inputArray).some((input) => input.checked);
             });
@@ -140,12 +139,12 @@ const displayForm = async () => {
         nextButton.addEventListener("click", () => {
             currentPage++;
             showPage(currentPage);
+            window.scrollTo({ top: 0, behavior: "smooth" });
         });
         formContainer.appendChild(nextButton);
 
         submitButton.addEventListener("click", async (e) => {
             e.preventDefault();
-            console.log("Form submitted");
 
             const selectedValues = [];
 
